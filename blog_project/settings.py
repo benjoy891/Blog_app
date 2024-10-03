55d0e83202af8c474a1304ee82630d2feaddbeff
Ben Joy <benjoybj891@gmail.com>
2024-10-03 10:00:04 +0530

    Next 4 commit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -160,7 +160,7 @@ MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), 
 ]
