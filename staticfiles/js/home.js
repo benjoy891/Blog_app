7675cfe40453ee4db8f5ffbf98b27464f433c328
Ben Joy <benjoybj891@gmail.com>
2024-10-03 09:46:13 +0530

    Next 3 commit

---
+++ staticfiles/js/home.js
@@ -0,0 +1,7 @@
+    document.addEventListener('DOMContentLoaded', () => {
+        const form = document.querySelector('form');
+        form.addEventListener('submit', () => {
+            // Display a notification or a modal here
+            alert("Post submitted successfully!");
+        });
+    });
